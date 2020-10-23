ba5c4a5c3c52e1436de19e67fcb8233c8337cf60
topheryun <46732742+topheryun@users.noreply.github.com>
2020-10-23 09:35:32 -0700

    second java friday commit

---
+++ JavaFriday/EMSMain.java
@@ -0,0 +1,16 @@
+package JavaFriday;
+
+public class EMSMain {
+
+	public static void main(String[] args) {
+		EMS revatureEmployee = new RevetureEmployer();
+		revatureEmployee.addEmployee();
+		revatureEmployee.updateEmployee();
+		revatureEmployee.getEmployee();
+		revatureEmployee.calcEmployeeSalary();
+		revatureEmployee.deleteEmployee();
+		revatureEmployee.commonEmployeeBenefits();
+
+	}
+
+}
