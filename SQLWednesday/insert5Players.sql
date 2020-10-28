2e35c3efcb69025b6d7a2c8f808b5258338cc62b
topheryun <46732742+topheryun@users.noreply.github.com>
2020-10-28 11:58:34 -0700

    SQL Wednesday

---
+++ SQLWednesday/insert5Players.sql
@@ -0,0 +1,7 @@
+insert into roc_myrevature.player 
+values
+(100, 'Sachin', 33, 'M', 'Rockers', 12346789),
+(101, 'Mahesh', 23, 'M', 'Bulls', 12345460),
+(102, 'Reena', 24, 'F', 'Bulls', 83612690),
+(103, 'Tina', 22, 'F', 'Riders', 12323231),
+(104, 'Rajesh', 22, 'M', 'Rockers', 12356490);
